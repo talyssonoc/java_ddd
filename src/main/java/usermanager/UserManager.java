d06644a4020b6b4e438e6fbdfc6bda0c79853f86
Talysson <talyssonoc@gmail.com>
2017-08-15 23:23:57 -0300

    Rename application and set correct development profile name

---
+++ src/main/java/usermanager/UserManager.java
@@ -0,0 +1,12 @@
+package usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManager {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserManager.class, args);
+  }
+}
